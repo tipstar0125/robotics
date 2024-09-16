dff5b015cf6be77e036f98d436e6d0cd8da76d55
Yoke <06106186@gmail.com>
2024-09-16 13:35:56 +0900

    add mvn

--- src/estimator.rs
+++ src/estimator.rs
@@ -1,30 +1,48 @@
 use crate::agent::Pose;
+use crate::common::Coord;
 use crate::move_::state_transition;
-use crate::multivariate_normal::MultivariateNormal;
+use crate::multivariate_normal::MyMultivariateNormal;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Particle {
     pub pose: Pose,
+    pub weight: f64,
 }
 
 impl Particle {
-    fn new(init_pose: Pose) -> Self {
-        Self { pose: init_pose }
+    fn new(init_pose: Pose, init_weight: f64) -> Self {
+        Self {
+            pose: init_pose,
+            weight: init_weight,
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct MotionNoiseStd {
+pub struct MotionNoise {
     pub nn: f64,
     pub no: f64,
     pub on: f64,
     pub oo: f64,
+    pub mvn: MyMultivariateNormal,
 }
 
-impl MotionNoiseStd {
-    pub fn to_vec(&self) -> Vec<f64> {
-        vec![self.nn, self.no, self.on, self.oo]
+impl MotionNoise {
+    pub fn new(nn: f64, no: f64, on: f64, oo: f64) -> Self {
+        Self {
+            nn,
+            no,
+            on,
+            oo,
+            mvn: MyMultivariateNormal::new_without_correlation(vec![0.0; 4], vec![nn, no, on, oo]),
+        }
     }
+}
+
+#[derive(Debug)]
+pub struct CameraNoise {
+    pub distance_rate: f64,
+    pub direction: f64,
 }
 
 #[derive(Debug)]
@@ -36,7 +54,7 @@ pub struct Estimator {
     pub time_interval: f64,
     pub radius: f64,
     pub particles: Vec<Particle>,
-    pub motion_noise_rate_pdf: MultivariateNormal,
+    pub motion_noise: MotionNoise,
     pub pose_records: Vec<Vec<Pose>>,
 }
 
@@ -48,7 +66,7 @@ impl Estimator {
         radius: f64,
         init_pose: Pose,
         particle_num: usize,
-        motion_noise: MotionNoiseStd,
+        motion_noise: MotionNoise,
     ) -> Self {
         Self {
             nu,
@@ -57,15 +75,15 @@ impl Estimator {
             prev_omega: 0.0,
             time_interval,
             radius,
-            particles: vec![Particle::new(init_pose); particle_num],
-            motion_noise_rate_pdf: MultivariateNormal::new(&motion_noise.to_vec()),
+            particles: vec![Particle::new(init_pose, 1.0_f64 / particle_num as f64); particle_num],
+            motion_noise,
             pose_records: vec![vec![init_pose; particle_num]],
         }
     }
     pub fn update_motion(&mut self, prev_nu: f64, prev_omega: f64) {
         let mut poses = vec![];
-        for p in self.particles.iter_mut() {
-            let ns = self.motion_noise_rate_pdf.sample();
+        for particle in self.particles.iter_mut() {
+            let ns = self.motion_noise.mvn.sample();
             let nn_noise = ns[0];
             let no_noise = ns[1];
             let on_noise = ns[2];
@@ -76,14 +94,21 @@ impl Estimator {
             let noised_omega = prev_omega
                 + on_noise * (prev_nu.abs() / self.time_interval).sqrt()
                 + oo_noise * (prev_omega.abs() / self.time_interval);
-            p.pose = state_transition(p.pose, noised_nu, noised_omega, self.time_interval);
-            poses.push(p.pose.clone());
+            particle.pose =
+                state_transition(particle.pose, noised_nu, noised_omega, self.time_interval);
+            poses.push(particle.pose.clone());
         }
         self.pose_records.push(poses);
     }
-    pub fn decision(&mut self) {
+    pub fn updater_observation(&mut self, landmarks: &[Coord]) {
+        for particle in self.particles.iter_mut() {
+            for mark in landmarks.iter() {}
+        }
+    }
+    pub fn decision(&mut self, landmarks: &[Coord]) {
         self.update_motion(self.prev_nu, self.prev_omega);
         self.prev_nu = self.nu;
         self.prev_omega = self.omega;
+        self.updater_observation(landmarks);
     }
 }
--- src/multivariate_normal.rs
+++ src/multivariate_normal.rs
@@ -1,27 +1,53 @@
-use nalgebra::{DMatrix, DVector, Matrix};
+use nalgebra::{DMatrix, DVector, Dyn, Matrix};
+use nalgebra_mvn::MultivariateNormal;
 use rand::{Rng, SeedableRng};
 use rand_distr::StandardNormal;
 use rand_pcg::Pcg64Mcg;
 
 #[derive(Debug)]
-pub struct MultivariateNormal {
+pub struct MyMultivariateNormal {
     pub n: usize,
     pub mu: DMatrix<f64>,
     pub l: DMatrix<f64>,
     pub rng: Pcg64Mcg,
+    pub mvn: MultivariateNormal<f64, Dyn>,
 }
 
-impl MultivariateNormal {
-    pub fn new(std: &Vec<f64>) -> Self {
-        let mu = DMatrix::from_vec(std.len(), 1, (0..std.len()).map(|_| 0.0_f64).collect());
-        let std2: Vec<_> = std.clone().iter().cloned().map(|x| x.powf(2.0)).collect();
-        let sig = DMatrix::from_diagonal(&DVector::from_row_slice(&std2));
+impl MyMultivariateNormal {
+    pub fn new(mu_vec: Vec<f64>, std_vec: Vec<f64>) -> Self {
+        assert!(mu_vec.len() * mu_vec.len() == std_vec.len());
+        let n = mu_vec.len();
+        let std2: Vec<_> = std_vec.iter().map(|x| x.powf(2.0)).collect();
+        let sig = DMatrix::from_row_slice(mu_vec.len(), 2, &std_vec);
+        let mvn =
+            MultivariateNormal::from_mean_and_covariance(&DVector::from_row_slice(&mu_vec), &sig)
+                .unwrap();
+        let mu = DMatrix::from_vec(mu_vec.len(), 1, mu_vec);
 
         Self {
-            n: std.len(),
+            n,
             mu,
             l: sig.cholesky().unwrap().l(),
             rng: Pcg64Mcg::seed_from_u64(0),
+            mvn,
+        }
+    }
+    pub fn new_without_correlation(mu_vec: Vec<f64>, std_vec: Vec<f64>) -> Self {
+        assert!(mu_vec.len() == std_vec.len());
+        let n = mu_vec.len();
+        let std2: Vec<_> = std_vec.iter().map(|x| x.powf(2.0)).collect();
+        let sig = DMatrix::from_diagonal(&DVector::from_row_slice(&std2));
+        let mvn =
+            MultivariateNormal::from_mean_and_covariance(&DVector::from_row_slice(&mu_vec), &sig)
+                .unwrap();
+        let mu = DMatrix::from_vec(mu_vec.len(), 1, mu_vec);
+
+        Self {
+            n,
+            mu,
+            l: sig.cholesky().unwrap().l(),
+            rng: Pcg64Mcg::seed_from_u64(0),
+            mvn,
         }
     }
     pub fn sample(&mut self) -> Vec<f64> {
@@ -35,5 +61,11 @@ impl MultivariateNormal {
         let sample = self.l.clone() * r + self.mu.clone();
         let ret: Vec<f64> = sample.iter().cloned().collect();
         ret
+    }
+    pub fn possibility(&self, xs_vec: Vec<f64>) -> f64 {
+        assert!(self.n == xs_vec.len());
+        let xs = DMatrix::from_row_slice(1, self.n, &xs_vec);
+        let result = self.mvn.pdf(&xs);
+        result[0]
     }
 }
